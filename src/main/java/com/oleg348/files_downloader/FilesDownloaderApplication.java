b842891b46d782acdd0a0e1f915b7ef24b93fe0c
Zherdev Oleg <zherdev96@gmail.com>
2021-02-02 22:56:04 +0300

    initialize project

---
+++ src/main/java/com/oleg348/files_downloader/FilesDownloaderApplication.java
@@ -0,0 +1,13 @@
+package com.oleg348.files_downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesDownloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oleg348/files_downloader/FilesDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleg348.files_downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
